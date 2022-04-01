e553e6a214030b5ac78c97e802331d0637403f5c
Lev Brouk <lev@mattermost.com>
2022-04-01 07:01:33 -0700

    customize port/rootURL in http mode

--- http-server/main.go
+++ http-server/main.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"go.uber.org/zap/zapcore"
 
+	"github.com/mattermost/mattermost-plugin-apps/apps"
 	"github.com/mattermost/mattermost-plugin-apps/utils"
 
+	root "github.com/mattermost/mattermost-app-servicenow"
 	"github.com/mattermost/mattermost-app-servicenow/function"
 )
 
 func main() {
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		portStr = "4445"
+	}
+
+	rootURL := os.Getenv("ROOT_URL")
+	if rootURL == "" {
+		rootURL = "http://localhost:" + portStr
+	}
+	root.AppManifest.Deploy.HTTP = &apps.HTTP{
+		RootURL: rootURL,
+	}
+
 	r := mux.NewRouter()
 	log := utils.MustMakeCommandLogger(zapcore.DebugLevel)
 	function.Init("http", r, log)
 	http.Handle("/", r)
 
-	listen := ":4445"
-	log.Infof("servicenow app started, manifest at http://localhost%s/manifest.json", listen)
+	listen := ":" + portStr
+	log.Infof("servicenow app started, listening on port %s, manifest at %s/manifest.json", portStr, rootURL)
 	panic(http.ListenAndServe(listen, nil))
 }
